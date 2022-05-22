bb4934df89d43d5d1c4162073fc03976013ff99e
Wesley Tavares <wesleyTavaresDev@outlook.com>
2022-05-22 19:10:55 -0300

    Jump

--- GameDev.tvGameJam/Assets/2.Scripts/Player/PlayerController.cs
+++ GameDev.tvGameJam/Assets/2.Scripts/Player/PlayerController.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float groundCheckLength;
+
+    bool isJumping;
 
     bool isLookingLeft;
 
     Rigidbody2D rb;
+    Animator anim;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        OnMove();
-    }
+        CheckGround();
 
+        OnMove();
+        Jump();
+    }
+#region Movementation
+    #region Move
     void OnMove()
     {
         float horizontal = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, rb.velocity.y);
+
+        anim.SetInteger("Movement", (int)rb.velocity.x);
 
         if(horizontal > 0 && isLookingLeft)
             Flip();
@@ -41,4 +52,28 @@ public class PlayerController : MonoBehaviour
 
         transform.localScale = scale;
     }
+    #endregion
+
+    void Jump()
+    {
+        anim.SetBool("Jumping", isJumping);
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isJumping)
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.fixedDeltaTime);
+        }
+    }
+
+    void CheckGround()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(rb.worldCenterOfMass, Vector2.down, groundCheckLength, 1 << 6);
+        Debug.DrawRay(rb.worldCenterOfMass, Vector2.down * groundCheckLength, Color.magenta);
+
+        if(ray.collider != null)
+            isJumping = false;
+        else
+            isJumping = true;
+    }
+
+#endregion
 }
